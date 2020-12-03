ef3202763759d1a7b52487dd0b568f482477669a
Petur Einarsson <petur.einarsson@bjss.com>
2020-12-03 08:52:03 +0000

    day 03 part 2 solved

--- day03/day03.py
+++ day03/day03.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 def read_input(path: str) -> list:
     with open(path) as f:
         return f.read().splitlines()
 
 
-def is_tree(input: list, y: int, x: int) -> bool:
-    if input[y][x] == "#":
+def is_tree(slope, x, y) -> bool:
+    if slope[y][x] == "#":
         return True
     return False
 
@@ -13,29 +16,39 @@ def broaden_slope(slope: list) -> list:
     return [line + line for line in slope]
 
 
-def count_trees(slope: list):
+def count_trees(slope: list, move_right=3, move_down=1):
     count = 0
-    current_y = 0
-    current_x = 0
+    current_x = move_right
+    current_y = move_down
     slope_length = len(slope)
     slope_width = len(slope[0])
 
     while slope_length > current_y:
-        if current_x > slope_width:
+        if current_x >= slope_width:
             slope = broaden_slope(slope)
             slope_width = len(slope[0])
 
-        if is_tree(slope, current_y, current_x):
+        if is_tree(slope, current_x, current_y):
             count += 1
-        current_y += 1
-        current_x += 3
+        current_x += move_right
+        current_y += move_down
 
     return count
 
 
+def traverse_multiple_slopes(slope) -> list:
+    to_traverse = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+    tree_list = []
+    for right, down in to_traverse:
+        number_of_trees = count_trees(slope, right, down)
+        tree_list.append(number_of_trees)
+    return tree_list
+
+
 if __name__ == "__main__":
     slope = read_input("input.txt")
+    print(f"Trees solution 1: {count_trees(slope)}")
 
-    number_of_trees = count_trees(slope)
-
-    print(number_of_trees)
+    multiple_trees = traverse_multiple_slopes(slope)
+    print(f"Trees solution 2: {np.prod(multiple_trees)}")
