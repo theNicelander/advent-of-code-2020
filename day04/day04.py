49911007884fc345d7bfe340a2ec0661ce83446b
Petur Einarsson <petur.einarsson@bjss.com>
2020-12-04 10:39:56 +0000

    day 4 part 2 solved

--- day04/day04.py
+++ day04/day04.py
@@ -31,11 +31,40 @@ def convert_passports_to_dictionaries(data):
     return passport_dicts
 
 
+def apply_part_1_validation(df):
+    """Ignore cid column & remove all other entries with missing values"""
+    df = df.drop("cid", axis=1).dropna()
+    print("Solution 1: ", len(df))
+    return df
+
+
+def hgt_check(hgt):
+    if hgt[-2:] == "cm":
+        return 150 <= int(hgt[:-2]) <= 193
+    if hgt[-2:] == "in":
+        return 59 <= int(hgt[:-2]) <= 76
+    return False
+
+
+def apply_part_2_validation(df):
+    valid_ecls = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    mask = (
+        df["byr"].astype(int).between(1920, 2002)
+        & df["iyr"].astype(int).between(2010, 2020)
+        & df["eyr"].astype(int).between(2020, 2030)
+        & df["hcl"].apply(lambda hcl: (len(hcl) == 7) & ("#" in hcl))
+        & df["pid"].apply(lambda pid: len(pid) == 9)
+        & df["ecl"].apply(lambda ecl: ecl in valid_ecls)
+        & df["hgt"].apply(hgt_check)
+    )
+    df = df[mask]
+    print("Solution 2: ", len(df))
+    return df
+
+
 if __name__ == "__main__":
     file = "input.txt"
 
     df = parse_input_to_df(file)
-    df = df.drop("cid", axis=1)
 
-    df_valid = df.dropna()
-    print(len(df_valid))
+    df.pipe(apply_part_1_validation).pipe(apply_part_2_validation)
