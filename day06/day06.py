b4365f1969f230db1d549ec55fdde770c24cb995
Petur Einarsson <petur.einarsson@bjss.com>
2020-12-06 09:03:40 +0000

    day 06 part 2 solved

--- day06/day06.py
+++ day06/day06.py
@@ -1,6 +1,3 @@
-from utils.files import read_groups_into_list
-from collections import Counter
-
 def read_file(path):
     with open(path) as f:
         return f.read().split("\n\n")
@@ -12,9 +9,29 @@ def sum_unique_answers(data: list) -> int:
     return sum(unique_answers)
 
 
+def sum_duplicate_answers(data):
+    number_of_intersections = [len(find_intersections(group)) for group in data]
+    return sum(number_of_intersections)
+
+
+def find_intersections(group):
+    for index, subgroup in enumerate(group.split("\n")):
+        if index == 0:
+            intersection = set(subgroup)
+        else:
+            intersection = intersection.intersection(set(subgroup))
+    return intersection
+
+
+def test_part_2():
+    data = read_file("test_part_2.txt")
+    assert sum_duplicate_answers(data) == 6
+    print("TEST PASSED")
 
 
 if __name__ == "__main__":
-    data = read_file("input.txt", clean_newline=False)
+    data = read_file("input.txt")
+    test_part_2()
 
     print("Part 1", sum_unique_answers(data))
+    print("Part 2", sum_duplicate_answers(data))
