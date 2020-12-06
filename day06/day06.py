007f52d254cdf1e569a5bdfe8273b6a7222ded39
Petur Einarsson <petur.einarsson@bjss.com>
2020-12-06 08:39:41 +0000

    day 06 part 1 solved

--- day06/day06.py
+++ day06/day06.py
@@ -1,5 +1,20 @@
 from utils.files import read_groups_into_list
+from collections import Counter
 
-if __name__ == '__main__':
-    data = read_groups_into_list("input.txt")
-    print(data[:100])
+def read_file(path):
+    with open(path) as f:
+        return f.read().split("\n\n")
+
+
+def sum_unique_answers(data: list) -> int:
+    data = [g.replace("\n", "") for g in data]
+    unique_answers = [len(set(group)) for group in data]
+    return sum(unique_answers)
+
+
+
+
+if __name__ == "__main__":
+    data = read_file("input.txt", clean_newline=False)
+
+    print("Part 1", sum_unique_answers(data))
